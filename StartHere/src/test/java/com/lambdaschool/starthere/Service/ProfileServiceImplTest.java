ec008920b184d1fd221a254db0e66ed2a7ac5b01
rushi444 <rushi44@gmail.com>
2019-08-01 11:03:49 -0700

    Creates ProfileServiceImplTest

--- StartHere/src/test/java/com/lambdaschool/starthere/Service/ProfileServiceImplTest.java
+++ StartHere/src/test/java/com/lambdaschool/starthere/Service/ProfileServiceImplTest.java
@@ -1,13 +1,17 @@
 package com.lambdaschool.starthere.Service;
 
 import com.lambdaschool.starthere.StartHereApplication;
+import com.lambdaschool.starthere.models.Profile;
 import com.lambdaschool.starthere.services.ProfileService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = StartHereApplication.class)
@@ -17,11 +21,24 @@ public class ProfileServiceImplTest {
     ProfileService profileService;
 
     @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void save() {
+        profileService.save(new Profile("Rushi", "Arumalla", "rushi44@gmail.com"), 5);
+        assertEquals("Rushi", profileService.findProfileById(5).getFirst_name());
 
     }
+
+//    @Test
+//    public void update() {
+//        profileService.save(new Profile("Sam", "Arumalla", "rushi44@gmail.com"), 6);
+//        profileService.update(profileService.findProfileById(6), 5);
+//        assertEquals("Bob", profileService.findProfileById(5).getFirst_name());
+//
+//    }
 
 
 }
--- StartHere/src/test/java/com/lambdaschool/starthere/StartHereApplication.java
+++ StartHere/src/test/java/com/lambdaschool/starthere/StartHereApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableWebMvc
+// @EnableWebMvc
 // @EnableJpaAuditing
 @SpringBootApplication
 @EnableSwagger2
